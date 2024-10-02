f35bef5a92958e5dab929bbd77f78467acaf0204
Vladut Alexandru Ciobotaru <ciobotarualexandru00@gmail.com>
2024-10-02 17:55:01 +0200

    navbar creta

---
+++ resources/views/components/main.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>DnD Market</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    </head>
+
+    <body>
+        
+        <x-navbar />
+
+        {{ $slot }}
+
+        <x-footer />
+
+    </body>
+
+</html>
+
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,29 @@
+<div class="container-fluid navbar-light mb-30">
+    <div class="row px-xl-5">
+        <div class="col-lg">
+            <ul class="nav nav-pills mb-3 d-flex" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" data-bs-toggle="pill" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" data-bs-toggle="pill" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Shop</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" data-bs-toggle="pill" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Carrello</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" data-bs-toggle="pill" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contattaci</a>
+                </li>
+
+                <div class="ms-auto d-flex">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" data-bs-toggle="pill" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Accedi</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" data-bs-toggle="pill" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Registrati</a>
+                    </li>
+                </div>
+            </ul>
+        </div>
+    </div>
+</div>
